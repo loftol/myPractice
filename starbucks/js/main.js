9feccdb65634dea89b60f127490d27c91b1db39e
hyte-pen <tkfkddl59323@naver.com>
2021-06-27 21:57:29 +0900

    starbucks header and visual

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -15,3 +15,30 @@ searchInputEl.addEventListener('blur', function(){
     searchInputEl.setAttribute('placeholder', '');
 });
 
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function(){
+    if(window.scrollY > 500){
+        //배지 숨기기
+        gsap.to(badgeEl,.6, {
+            opacity: 0,
+            display : 'none'
+        })
+    }
+    else{
+        gsap.to(badgeEl,.6,{
+            opacity: 1,
+            display : 'block'
+        })
+    }
+},300));
+
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function(fadeEl,index){
+    // gsap.to(요소, 지속시간, ㅇㅂ)
+    gsap.to(fadeEl, 1,{
+        delay: (index + 1) * .7,
+        opacity: 1
+    });
+});
