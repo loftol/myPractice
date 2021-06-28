5186c975542636f93ee014c532f8fb168d7ec2b7
hyte-pen <tkfkddl59323@naver.com>
2021-06-28 21:41:17 +0900

    add slide (promotion)

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -36,9 +36,48 @@ window.addEventListener('scroll', _.throttle(function(){
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function(fadeEl,index){
-    // gsap.to(요소, 지속시간, ㅇㅂ)
+    // gsap.to(요소, 지속시간, 옵션)
     gsap.to(fadeEl, 1,{
         delay: (index + 1) * .7,
         opacity: 1
     });
+});
+
+// new Swiper(선택자, 옵션)
+new Swiper('.notice-line .swiper-container',{
+    direction: 'vertical',
+    autoplay: true,
+    loop: true
+});
+
+new Swiper('.promotion .swiper-container',{
+    slidesPerView: 3,
+    spaceBetween: 10,
+    centeredSlides: true,
+    loop: true,
+    autoplay: {
+        delay: 5000
+    },
+    pagination: {
+        el: '.promotion .swiper-pagination',
+        clickable: true
+    },
+    navigation: {
+        prevEl: '.promotion .swiper-prev',
+        nextEl: '.promotion .swiper-next'
+    }
+});
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function () {
+    isHidePromotion = !isHidePromotion;
+    if (isHidePromotion) {
+        promotionEl.classList.add('hide');
+    }
+    else{
+        promotionEl.classList.remove('hide');
+    }
 });
