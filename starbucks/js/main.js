51c1e1024303a71dea3f46e29190fd78ddc96730
hyte-pen <tkfkddl59323@naver.com>
2021-07-12 15:50:33 +0900

    starbucks page complete

--- starbucks/js/main.js
+++ starbucks/js/main.js
@@ -16,6 +16,7 @@ searchInputEl.addEventListener('blur', function(){
 });
 
 const badgeEl = document.querySelector('header .badges');
+const toTopEl = document.querySelector('#to-top');
 
 window.addEventListener('scroll', _.throttle(function(){
     if(window.scrollY > 500){
@@ -23,16 +24,28 @@ window.addEventListener('scroll', _.throttle(function(){
         gsap.to(badgeEl,.6, {
             opacity: 0,
             display : 'none'
-        })
+        });
+        gsap.to(toTopEl, .2, {
+            x: 0
+        });
     }
     else{
         gsap.to(badgeEl,.6,{
             opacity: 1,
             display : 'block'
-        })
+        });
+        gsap.to(toTopEl, .2,{
+            x: 100
+        });
     }
 },300));
 
+
+toTopEl.addEventListener('click',function () {
+    gsap.to(window,.7, {
+        scrollTo: 0
+    });
+});
 
 const fadeEls = document.querySelectorAll('.visual .fade-in');
 fadeEls.forEach(function(fadeEl,index){
@@ -68,6 +81,17 @@ new Swiper('.promotion .swiper-container',{
     }
 });
 
+new Swiper('.awards .swiper-container',{
+    autoplay: true,
+    loop: true,
+    spaceBetween: 30,
+    slidesPerView: 5,
+    navigation: {
+        prevEl: '.awards .swiper-prev',
+        nextEl: '.awards .swiper-next'
+    }
+});
+
 const promotionEl = document.querySelector('.promotion');
 const promotionToggleBtn = document.querySelector('.toggle-promotion');
 let isHidePromotion = false;
@@ -81,3 +105,36 @@ promotionToggleBtn.addEventListener('click', function () {
         promotionEl.classList.remove('hide');
     }
 });
+
+function random(min, max) {
+    // `.toFixed()`를 통해 반환된 문자 데이터를,
+    // `parseFloat()`을 통해 소수점을 가지는 숫자 데이터로 변환
+    return parseFloat((Math.random() * (max - min) + min).toFixed(2))
+  }
+
+function floatingObject(selector, delay, size) {
+    //gsap.to(요소, 시간, 옵션);
+    gsap.to(selector, random(1.5, 2.5), {
+        y: size,
+        repeat: -1,
+        yoyo: true,
+        ease: Power1.easeInOut,
+        delay: random(0, delay)
+    });
+}
+floatingObject('.floating1', 1, 15);
+floatingObject('.floating2', .5, 15);
+floatingObject('.floating3', 1.5, 20);
+
+const spyEls = document.querySelectorAll('section.scroll-spy');
+spyEls.forEach(function(spyEl) {
+    new ScrollMagic.Scene({
+        triggerElement: spyEl,
+        triggerHook: .8
+    })
+    .setClassToggle(spyEl, 'show')
+    .addTo(new ScrollMagic.Controller());
+});
+
+const thisYear = document.querySelector('.this-year');
+thisYear.textContent = new Date().getFullYear(); // 2021
